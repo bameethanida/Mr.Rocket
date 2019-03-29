64767fb8a23f9517bb8c5efed27ddad243963331
bameethanida <bameejrn@outlook.com>
2019-03-29 13:24:51 +0700

    update

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import arcade
-from models import Ship,World,Meteorite
+from models import Ship,World,Alien_A,Alien_B
+
 SCREEN_WIDTH = 1400
 SCREEN_HEIGHT = 750
- 
+
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
+
         super().__init__(width, height)
- 
-        arcade.set_background_color(arcade.color.BLACK)
- 
+
+        self.background = None
         self.world = World(width, height)
-        self.ship_sprite = ModelSprite('images/dot.png',model=self.world.ship)
-        self.meteorite_sprite = ModelSprite('images/dot.png',model=self.world.meteorite)
-        
- 
- 
+        self.ship_sprite = ModelSprite('images/ship.png',model=self.world.ship)
+        self.alien_A_sprite = ModelSprite('images/alienA.png',model=self.world.alien_A)
+        self.alien_B_sprite = ModelSprite('images/alienB.png',model=self.world.alien_B)
+
+    def set_up(self):
+
+        self.background = arcade.load_texture("images/background.jpg")
+
     def on_draw(self):
+
         arcade.start_render()
+
+        arcade.draw_texture_rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         self.ship_sprite.draw()
-        self.meteorite_sprite.draw()
- 
+        self.alien_A_sprite.draw()
+        self.alien_B_sprite.draw()
+    
     def update(self, delta):
         self.world.update(delta)
 
@@ -28,7 +36,7 @@ class SpaceGameWindow(arcade.Window):
 
     def on_mouse_motion(self, x, y, dx, dy):
         self.world.on_mouse_motion(x, y, dx, dy)
-        
+
 class ModelSprite(arcade.Sprite):
     def __init__(self, *args, **kwargs):
         self.model = kwargs.pop('model', None)
@@ -45,4 +53,5 @@ class ModelSprite(arcade.Sprite):
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window.set_up()
     arcade.run()
--- models.py
+++ models.py
@@ -5,7 +5,6 @@ SCREEN_WIDTH = 1400
 SCREEN_HEIGHT = 750
 
 class Ship:
- 
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
@@ -15,12 +14,12 @@ class Ship:
     def update(self, delta):
         pass
 
-class Meteorite:
+class Alien_A:
     def __init__(self, world, x, y, speed=0):
         self.world = world
         self.x = x
         self.y = y
-        self.speed = 10
+        self.speed = 1
 
     def update(self, delta):
         if (self.x < 0):
@@ -28,7 +27,18 @@ class Meteorite:
             self.y = randint(50, SCREEN_HEIGHT - 50)
         self.x -= self.speed
         
+class Alien_B:
+    def __init__(self, world, x, y, speed=0):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.speed = 3
 
+    def update(self, delta):
+        if (self.x < 0):
+            self.x = self.world.width - 1
+            self.y = randint(50, SCREEN_HEIGHT - 50)
+        self.x -= self.speed
         
 class World:
     def __init__(self, width, height):
@@ -36,14 +46,14 @@ class World:
         self.height = height
  
         self.ship = Ship(self, 100, 100)
-        self.meteorite = Meteorite(self, SCREEN_WIDTH - 1, randint(50, SCREEN_HEIGHT - 50))
+        self.alien_A = Alien_A(self, SCREEN_WIDTH - 1, randint(50, SCREEN_HEIGHT - 50))
+        self.alien_B = Alien_B(self, SCREEN_WIDTH - 1, randint(50, SCREEN_HEIGHT - 50))
 
     
     def on_key_press(self, key, key_modifiers):
         pass
 
     def on_mouse_motion(self, x, y, dx, dy):
-        """ Called to update our objects. Happens approximately 60 times per second."""
         if (self.ship.y >= 50 and self.ship.y <= 700):
             self.ship.y = y
         elif (self.ship.y < 50):
@@ -53,4 +63,5 @@ class World:
  
     def update(self, delta):
         self.ship.update(delta)
-        self.meteorite.update(delta)
+        self.alien_A.update(delta)
+        self.alien_B.update(delta)
