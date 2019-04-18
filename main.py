de62359648aa88cf764a02f8eac34f423a364f12
bameethanida <bameejrn@outlook.com>
2019-04-18 14:33:01 +0700

    moving background

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 import arcade
-from models import Ship,World,Alien_A,Alien_B,Bullet
+from models import Ship,World,Alien_A,Alien_B,Bullet,Background
 
 SCREEN_WIDTH = 1400
 SCREEN_HEIGHT = 750
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+ 
+        super().__init__(*args, **kwargs)
+ 
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+ 
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
 
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
 
         super().__init__(width, height)
 
-        self.background = None
         self.world = World(width, height)
+        self.background_sprite = ModelSprite('images/background.png', model=self.world.background)
+        self.background_sprite_2 = ModelSprite('images/background2.png', model=self.world.background2)
         self.ship_sprite = ModelSprite('images/ship.png',model=self.world.ship)
         self.alien_A_sprite = ModelSprite('images/alienA.png',model=self.world.alien_A)
         self.alien_B_sprite = ModelSprite('images/alienB.png',model=self.world.alien_B)
         self.bullet = ModelSprite('images/bullet.png', model=self.world.bullet)
 
-    def set_up(self):
-
-        self.background = arcade.load_texture("images/background.jpg")
 
     def on_draw(self):
 
         arcade.start_render()
-        arcade.draw_texture_rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        self.background_sprite.draw()
+        self.background_sprite_2.draw()
         self.ship_sprite.draw()
         self.alien_A_sprite.draw()
         self.alien_B_sprite.draw()
@@ -40,21 +53,11 @@ class SpaceGameWindow(arcade.Window):
     def on_mouse_motion(self, x, y, dx, dy):
         self.world.on_mouse_motion(x, y, dx, dy)
 
-class ModelSprite(arcade.Sprite):
-    def __init__(self, *args, **kwargs):
-        self.model = kwargs.pop('model', None)
- 
-        super().__init__(*args, **kwargs)
- 
-    def sync_with_model(self):
-        if self.model:
-            self.set_position(self.model.x, self.model.y)
- 
-    def draw(self):
-        self.sync_with_model()
-        super().draw()
 
-if __name__ == '__main__':
+
+def main():
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
-    window.set_up()
     arcade.run()
+    
+if __name__ == '__main__':
+    main()
--- models.py
+++ models.py
@@ -4,12 +4,22 @@ from random import randint
 SCREEN_WIDTH = 1400
 SCREEN_HEIGHT = 750
 
+BACKGROUND_SPEED = 2
+
+class Background:
+    def __init__(self, world, x,y):
+        self.world = world
+        self.x = x
+        self.y = y
+
+    def update(self, delta):
+        self.x -= BACKGROUND_SPEED
+
 class Ship:
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
- 
  
     def update(self, delta):
         pass
@@ -68,15 +78,23 @@ class Bullet:
 
 class World:
     def __init__(self, width, height):
+
         self.width = width
         self.height = height
- 
+        self.background = Background(self,700,375)
+        self.background2 = Background(self,2100,375)
         self.ship = Ship(self, 100, 100)
         self.alien_A = Alien_A(self, SCREEN_WIDTH - 1, randint(50, SCREEN_HEIGHT - 50))
         self.alien_B = Alien_B(self, SCREEN_WIDTH - 1, randint(50, SCREEN_HEIGHT - 50))
         self.bullet = Bullet(self, 150, 130)
 
-    
+    def moving_background(self):
+        if self.background.x == -700:
+            self.background.x = 2100
+
+        if self.background2.x == -700:
+            self.background2.x = 2100 
+
     def on_key_press(self, key, key_modifiers):
         pass
 
@@ -98,6 +116,9 @@ class World:
             self.ship.y = 700
  
     def update(self, delta):
+        self.background.update(delta)
+        self.background2.update(delta)
+        self.moving_background()
         self.ship.update(delta)
         self.alien_A.update(delta)
         self.alien_B.update(delta)
